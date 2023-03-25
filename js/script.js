91d9335f07c43fe984cf5c499aae7aa52adc203f
Nancy Asiuloka <nancyasiuloka@gmail.com>
2023-03-25 10:27:22 +0100

    added css

--- js/script.js
+++ js/script.js
@@ -1,9 +1,15 @@
-const toggle = document.querySelector('.toggle');
+const toggle = document.querySelector(".toggle");
 
-const navigation = document.querySelector('.navigation');
+const navigation = document.querySelector(".navigation");
 
+toggle.addEventListener("click", () => {
+  toggle.classList.toggle("active");
+  navigation.classList.toggle("active");
+});
 
-toggle.addEventListener('click', () => {
-    toggle.classList.toggle('active')
-    navigation.classList.toggle('active')
-})
+function add(a, b) {
+  return a + b;
+}
+
+let sum = add(2,4)
+console.log(sum)
